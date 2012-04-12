70842196ea95016ff2df33b4a9650bdfd468ebac
HAMADA,Satoshi(mcatm) <tkcs@pelepop.com>
2012-04-12 17:27:58 +0900

    metabox test

--- wp-schedule-manager.php
+++ wp-schedule-manager.php
@@ -9,6 +9,8 @@ Author URI: http://www.cinra.co.jp/
 License: not-yet
 */
 
+include('./core.php');
+
 class wp_schedule_manager {
 	
 	public function get($usr_opt = array()) {
@@ -34,46 +36,7 @@ class wp_schedule_manager {
 	}
 }
 
-function wpsm_init_adminmenu() {
-	add_meta_box('schedule_manager', __('schedule'), 'wpsm_output_metabox', 'post', 'normal');
-}
-add_action('admin_menu', 'wpsm_init_adminmenu');
 
-function wpsm_output_metabox() {
-	echo <<<EOF
-
-<style type="text/css">
-a.wpsm_plus {
-	background:#333;
-}
-a.wpsm_plus:hover {
-	cursor:pointer;
-}
-</style>
-
-<script type="text/javascript">
-(function($) {
-	$(function() {
-		$('.wpsm_plus').live('click', function() {
-			var box = $(this);
-			//box.parent().append(box.get(0));
-			box.parent().parent().append(box.get(0));
-			console.log(box.get(0));
-		});
-	});
-})(jQuery);
-</script>
-
-<div class="wpsm_daybox">
-	<p>がはははははは</p>
-	<p><a class="wpsm_plus">+</a></p>
-</div>
-
-
-
-
-EOF;
-}
 
 if(defined('WP_PLUGIN_URL')) {
 	
