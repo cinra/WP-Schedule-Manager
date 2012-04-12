0d1bc265843b59f4cd2f5d7533f4dc15980ddefc
HAMADA,Satoshi(mcatm) <tkcs@pelepop.com>
2012-04-12 17:23:09 +0900

    metabox test

--- wp-schedule-manager.php
+++ wp-schedule-manager.php
@@ -40,7 +40,39 @@ function wpsm_init_adminmenu() {
 add_action('admin_menu', 'wpsm_init_adminmenu');
 
 function wpsm_output_metabox() {
-	echo '<div>テストです</div>';
+	echo <<<EOF
+
+<style type="text/css">
+a.wpsm_plus {
+	background:#333;
+}
+a.wpsm_plus:hover {
+	cursor:pointer;
+}
+</style>
+
+<script type="text/javascript">
+(function($) {
+	$(function() {
+		$('.wpsm_plus').live('click', function() {
+			var box = $(this);
+			//box.parent().append(box.get(0));
+			box.parent().parent().append(box.get(0));
+			console.log(box.get(0));
+		});
+	});
+})(jQuery);
+</script>
+
+<div class="wpsm_daybox">
+	<p>がはははははは</p>
+	<p><a class="wpsm_plus">+</a></p>
+</div>
+
+
+
+
+EOF;
 }
 
 if(defined('WP_PLUGIN_URL')) {
