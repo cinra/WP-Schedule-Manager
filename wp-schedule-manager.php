df070b089727e329c5aeccdf5babade38bda5f07
TOKISATO, Mitsuru <tokisatomitsuru@gmail.com>
2012-04-13 16:26:35 +0900

    br

--- wp-schedule-manager.php
+++ wp-schedule-manager.php
@@ -31,29 +31,58 @@ class wp_schedule_manager {
 		}
 	}
 	
+	public function set($usr_opt = array()) {
+		
+		$opt = array(
+			'post_id'	=> get_the_ID()
+		);
+		if (is_array($usr_opt)) $opt = array_merge($opt, $usr_opt);
+		
+		#echo 'POSTID: '.$opt['post_id'];
+		#print_r($_POST);
+		
+		//add_post_custom();
+		
+		exit();
+	}
+	
 	function __construct() {
 		//exit(dirname(__FILE__));
 	}
 }
 
+add_action('save_post', array(&$wpsm, 'set'), 10);
+
 add_action('admin_menu', 'wpsm_add_sidemenu');
 function wpsm_add_sidemenu() {
-	
 	add_menu_page('schedule', 'schedule', 7, 'wpsm', 'admin_schedule_list');
  	add_submenu_page('wpsm', 'schedule', 7, 'wpsm', 'admin_schedule_list');
-	
 }
 
 function admin_schedule_list() {
+	
+	$post = get_posts();
+	
+	$options = "";
+	foreach ($post as $p) {
+		$options .= '<option value="'.$p->ID.'">'.$p->post_title.'</option>';
+	}
+	
 	echo '<div class="wrap">';
 	echo '<div id="icon-options-general" class="icon32"><br></div>';
 	echo "<h2>スケジュール</h2>";
 	echo <<<EOF
-<div class="subsubsub"></div>
+<div class="subsubsub">前の月 | 次の月</div>
 
 <table class="widefat" style="margin-bottom: 1em;">
 	<tr>
-		<td>UUUUUU</td>
+		<td>
+			<select>
+EOF;
+	echo $options;
+	echo <<<EOF
+			</select>
+		</td>
 	</tr>
 </table>
 
@@ -61,17 +90,58 @@ function admin_schedule_list() {
 <table class="widefat">
 	<thead>
 		<tr>
-			<th scope="col" colspan="2">テスト</th>
+			<th scope="col" colspan="25">YYYY.MM.DD</th>
+		</tr>
+		<tr>
+			<th scope="col" colspan="1">タイトル</th>
+			<th scope="col" colspan="1">日にち</th>
+			<th scope="col" colspan="1">時間</th>
+			<th scope="col" colspan="1">受付の有無</th>
+			<th scope="col" colspan="1">受付URL</th>
 		</tr>
 	</thead>
 	<tbody>
 		<tr class="mainraw">
-			<td>テスト</td>
-			<td>テスト</td>
+			<td>骸骨君物語</td>
+			<td>12.01.01</td>
+			<td>16:20</td>
+			<td>有</td>
+			<td>http://www.yahoo.com</td>
 		</tr>
 		<tr class="mainraw">
-			<td>テスト</td>
-			<td>テスト</td>
+			<td>肋骨君物語</td>
+			<td>12.03.01</td>
+			<td>11:20</td>
+			<td>有</td>
+			<td>http://www.yahoo.com</td>
+		</tr>
+		<tr class="mainraw">
+			<td>足骨君物語</td>
+			<td>12.02.01</td>
+			<td>16:20</td>
+			<td>有</td>
+			<td>http://www.yahoo.com</td>
+		</tr>
+		<tr class="mainraw">
+			<td>鎖骨君物語</td>
+			<td>12.05.01</td>
+			<td>16:20</td>
+			<td>有</td>
+			<td>http://www.yahoo.com</td>
+		</tr>
+		<tr class="mainraw">
+			<td>背骨君物語</td>
+			<td>12.01.11</td>
+			<td>16:25</td>
+			<td>有</td>
+			<td>http://www.yahoo.com</td>
+		</tr>
+		<tr class="mainraw">
+			<td>肋骨君物語</td>
+			<td>11.01.01</td>
+			<td>06:20</td>
+			<td>有</td>
+			<td>http://www.yahoo.com</td>
 		</tr>
 	</tbody>
 </table>
