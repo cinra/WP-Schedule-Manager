47c36e973272530bc6c77bf58850510a08cb25b3
HAMADA,Satoshi(mcatm) <tkcs@pelepop.com>
2012-04-13 17:18:49 +0900

    work files

--- wp-schedule-manager.php
+++ wp-schedule-manager.php
@@ -11,73 +11,11 @@ License: not-yet
 
 include(dirname(__FILE__).'/core.php');
 
-class wp_schedule_manager {
-	
-	public function get($usr_opt = array()) {
-		
-		global $wpdb;
-		
-		$opt = array(
-			'post_id'	=> false,
-			'date'		=> false
-		);
-		$opt = array_merge($opt, $usr_opt);
-		
-		$where = array();
-		
-		if ($opt['post_id']) $where[] = '`post_id` = '.$opt['post_id'];
-		
-		$sql = "SELECT * FROM ".WPSM_DB_TABLENAME." WHERE ".implode(' AND ', $where);
-		#exit($sql);
-		print_r($wpdb->get_results($sql));
-		exit();
-		
-		// Test
-		if (!is_admin()) {
-			#print_r($opt);
-			
-			echo '<br />'.strtotime($opt['date']);
-			echo '<br />'.time();
-			
-			#exit;
-		}
-	}
-	
-	public function set($usr_opt = array()) {
-		
-		global $wpdb;
-		
-		$opt = array(
-			'post_id'	=> get_the_ID()
-		);
-		if (is_array($usr_opt)) $opt = array_merge($opt, $usr_opt);
-		
-		$wpdb->query("DELETE FROM ".WPSM_DB_TABLENAME." WHERE post_id = '".$opt['post_id']."'");
-		foreach($_POST['wpsm_day'] as $k => $d){
-			$wpdb->insert('wp_wpsm_cal', array(
-				'date'			=> $_POST['wpsm_day'][$k],
-				'time'			=> $_POST['wpsm_time'][$k],
-				'description'	=> $_POST['wpsm_description'][$k],
-				'url'			=> $_POST['wpsm_url'][$k],
-				'post_id'		=> $opt['post_id']
-			));
-		}
-	}
-	
-	function __construct() {
-		
-		if (!defined('WPSM_DB_TABLENAME')) define('WPSM_DB_TABLENAME', 'wp_wpsm_cal');//table name
-		
-		
-		//exit(dirname(__FILE__));
-	}
-}
-
 add_action('save_post', array(&$wpsm, 'set'), 10);
 
 add_action('admin_menu', 'wpsm_add_sidemenu');
 function wpsm_add_sidemenu() {
-	add_menu_page('schedule', 'schedule', 7, 'wpsm', 'admin_schedule_list');
+	add_menu_page('schedule', 'スケジュール', 7, 'wpsm', 'admin_schedule_list');
  	add_submenu_page('wpsm', 'schedule','編集', 7, 'edit', 'admin_schedule_list');
 }
 
@@ -171,6 +109,121 @@ EOF;
 
 EOF;
 	echo '</div>';
+}
+
+
+
+
+
+function wpsm_init_adminmenu() {
+	add_meta_box('schedule_manager', __('schedule'), 'wpsm_output_metabox', 'post', 'normal');
+}
+add_action('admin_menu', 'wpsm_init_adminmenu');
+
+function wpsm_output_metabox() {
+	echo <<<EOF
+
+<style type="text/css">
+a.wpsm_plus {
+	background:#333;
+}
+a.wpsm_plus:hover {
+	cursor:pointer;
+}
+a.wpsm_maenas {
+	background:#333;
+	display:none;
+}
+a.wpsm_maenas:hover {
+	cursor:pointer;
+}
+</style>
+
+<script type="text/javascript">
+(function($) {
+
+
+	$(function() {
+	 var count=0;
+	 var alcount=0;
+	 var wpsm="wpsm_daybox";
+	 var wpsmaf="wpsm_daybox0";
+		$('.wpsm_plus').live('click', function() {
+			var html =$('.wpsm_daybox0').html();
+			count++;
+			alcount++;
+			wpsm=wpsm+count;
+			//console.log(html);
+			var wpsmadd="<div class=" + wpsm + "></div>";
+			wpsmaf="."+ wpsmaf;
+			$(wpsmaf).after(wpsmadd);
+			$("."+wpsm).append(html);
+			//console.log($("."+wpsm).children());
+			wpsm_day="wpsm_day[" + count + "]";
+			wpsm_time="wpsm_time[" + count + "]";
+			wpsm_yoyaku="wpsm_yoyaku[" + count + "]";
+			wpsm_URL="wpsm_url[" + count + "]";
+			//$("."+wpsm).children(".day").children("input").attr("name",wpsm_day);
+			$("."+wpsm).children(".day").children("input").attr({name:wpsm_day,value:""});
+
+			$("."+wpsm).children(".time").children("input").attr({name:wpsm_time,value:""});
+			$("."+wpsm).children(".yoyaku").children("input").attr({name:wpsm_yoyaku,value:0});
+			$("."+wpsm).children(".URL").children("input").attr({name:wpsm_URL,value:""});
+			$(".wpsm_maenas").css("display","inline");
+			
+			
+			
+			//
+			wpsmaf=wpsm;
+			wpsm="wpsm_daybox";
+						
+
+		});
+		$('.wpsm_maenas').live('click', function() {
+			console.log($(this).parent().parent());
+			$(this).parent().parent().css("display","none");
+			$(this).parent().parent().children(".day").children("input").attr("value","delete");
+			$(this).parent().parent().children(".time").children("input").attr("value","delete");
+			$(this).parent().parent().children(".yoyaku").children("input").attr("value","0");
+			$(this).parent().parent().children(".URL").children("input").attr("value","delete");
+						
+			alcount--;
+			console.log(alcount);
+			
+			if(1>alcount){
+				$(".wpsm_maenas").css("display","none");
+			}
+			
+			
+		});
+	});
+})(jQuery);
+</script>
+
+<div class="wpsm_daybox0">
+	<p class="day">
+	<label >日付</label>
+	<input type="text" name="wpsm_day[0]" size="50" tabindex="1" value="" id="sc-data" autocomplete="off">
+	</p>
+	<p class="time">
+	<label >時間</label>
+	<input type="text" name="wpsm_time[0]" size="50" tabindex="1" value="" id="sc-time" autocomplete="off">
+	</p>
+	<p class="yoyaku">
+	<label >予約可</label>
+	<input type="checkbox" name="wpsm_yoyaku[0]" value="0">
+	</p>
+	<p class="URL">
+	<label >URL</label>
+	<input type="text" name="wpsm_url[0]" size="100" tabindex="1" value="" id="sc-URL" autocomplete="off">
+	<p><a class="wpsm_plus">+</a></p>
+	<p><a class="wpsm_maenas">-</a></p>
+</div>
+
+
+
+
+EOF;
 }
 
 
