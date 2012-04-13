3e23f0e32f48970ecdc8c4064e408be8c3d0d407
HAMADA,Satoshi(mcatm) <tkcs@pelepop.com>
2012-04-13 17:10:08 +0900

    $wpsm->get

--- wp-schedule-manager.php
+++ wp-schedule-manager.php
@@ -14,39 +14,61 @@ include(dirname(__FILE__).'/core.php');
 class wp_schedule_manager {
 	
 	public function get($usr_opt = array()) {
+		
+		global $wpdb;
+		
 		$opt = array(
-			'date'		=> 'Today'
+			'post_id'	=> false,
+			'date'		=> false
 		);
 		$opt = array_merge($opt, $usr_opt);
 		
+		$where = array();
+		
+		if ($opt['post_id']) $where[] = '`post_id` = '.$opt['post_id'];
+		
+		$sql = "SELECT * FROM ".WPSM_DB_TABLENAME." WHERE ".implode(' AND ', $where);
+		#exit($sql);
+		print_r($wpdb->get_results($sql));
+		exit();
 		
 		// Test
 		if (!is_admin()) {
-			print_r($opt);
+			#print_r($opt);
 			
 			echo '<br />'.strtotime($opt['date']);
 			echo '<br />'.time();
 			
-			exit;
+			#exit;
 		}
 	}
 	
 	public function set($usr_opt = array()) {
+		
+		global $wpdb;
 		
 		$opt = array(
 			'post_id'	=> get_the_ID()
 		);
 		if (is_array($usr_opt)) $opt = array_merge($opt, $usr_opt);
 		
-		#echo 'POSTID: '.$opt['post_id'];
-		#print_r($_POST);
-		
-		//add_post_custom();
-		
-		exit();
+		$wpdb->query("DELETE FROM ".WPSM_DB_TABLENAME." WHERE post_id = '".$opt['post_id']."'");
+		foreach($_POST['wpsm_day'] as $k => $d){
+			$wpdb->insert('wp_wpsm_cal', array(
+				'date'			=> $_POST['wpsm_day'][$k],
+				'time'			=> $_POST['wpsm_time'][$k],
+				'description'	=> $_POST['wpsm_description'][$k],
+				'url'			=> $_POST['wpsm_url'][$k],
+				'post_id'		=> $opt['post_id']
+			));
+		}
 	}
 	
 	function __construct() {
+		
+		if (!defined('WPSM_DB_TABLENAME')) define('WPSM_DB_TABLENAME', 'wp_wpsm_cal');//table name
+		
+		
 		//exit(dirname(__FILE__));
 	}
 }
