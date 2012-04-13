fb395fd9fddf1294f644bbcc41fa373ce07af852
HAMADA,Satoshi(mcatm) <tkcs@pelepop.com>
2012-04-13 11:53:23 +0900

    first-branch

--- wp-schedule-manager.php
+++ wp-schedule-manager.php
@@ -39,23 +39,44 @@ class wp_schedule_manager {
 add_action('admin_menu', 'wpsm_add_sidemenu');
 function wpsm_add_sidemenu() {
 	
-	add_menu_page('schedule', 'schedule', 7, 'wpsm', 'toppage');
- 	add_submenu_page('wpsm', 'schedule', 7, 'wpsm', 'toppage');
+	add_menu_page('schedule', 'schedule', 7, 'wpsm', 'admin_schedule_list');
+ 	add_submenu_page('wpsm', 'schedule', 7, 'wpsm', 'admin_schedule_list');
 	
 }
 
-function toppage() {
+function admin_schedule_list() {
 	echo '<div class="wrap">';
 	echo '<div id="icon-options-general" class="icon32"><br></div>';
 	echo "<h2>スケジュール</h2>";
 	echo <<<EOF
 <div class="subsubsub"></div>
 
-<table class="widefat">
+<table class="widefat" style="margin-bottom: 1em;">
 	<tr>
 		<td>UUUUUU</td>
 	</tr>
 </table>
+
+<form>
+<table class="widefat">
+	<thead>
+		<tr>
+			<th scope="col" colspan="2">テスト</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr class="mainraw">
+			<td>テスト</td>
+			<td>テスト</td>
+		</tr>
+		<tr class="mainraw">
+			<td>テスト</td>
+			<td>テスト</td>
+		</tr>
+	</tbody>
+</table>
+</form>
+
 EOF;
 	echo '</div>';
 }
